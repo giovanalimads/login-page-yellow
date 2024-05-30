5549efbca2288b05fbb36893c9c7f560817921df
giovanalimads <gilimaolliveira@gmail.com>
2024-05-29 22:10:09 -0300

    commit

--- loginpage.js
+++ loginpage.js
@@ -1,3 +1,4 @@
+// função mostrar/esconder senhar
 const show_pw_btn = document.querySelector('#show-passwd');
 const show_pw_icon = document.querySelector('#show-passwd img');
 const pw_input = document.querySelector('#password');
